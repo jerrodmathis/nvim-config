af011c51f0a2583eda65f10a03c5d66055d61335
Jerrod Mathis <jerrodmathis@proton.me>
2025-03-17 22:15:13 -0400

    feat: adding nvim-tree, updating colors

--- lua/jmathis/plugins/colors.lua
+++ lua/jmathis/plugins/colors.lua
@@ -4,8 +4,17 @@ return {
         lazy = false,
         priority = 1000,
         opts = {},
+        -- config = function()
+        --     vim.cmd([[colorscheme tokyonight-storm]])
+        -- end
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
         config = function()
-            vim.cmd([[colorscheme tokyonight-storm]])
+            vim.cmd([[colorscheme catppuccin-mocha]])
         end
     },
 }
