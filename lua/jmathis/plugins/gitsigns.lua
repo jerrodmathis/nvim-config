19f845d4d95c6972c96d4039fba67a8a56f48019
Jerrod Mathis <jerrodmathis@proton.me>
2025-04-02 10:16:35 -0400

    feat: updating gitsigns config

--- lua/jmathis/plugins/gitsigns.lua
+++ lua/jmathis/plugins/gitsigns.lua
@@ -2,11 +2,29 @@ return {
   'lewis6991/gitsigns.nvim',
   opts = {
     signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
+      add = { text = '' },
+      change = { text = '' },
+      delete = { text = '' },
       topdelete = { text = '‾' },
-      changedelete = { text = '~' },
+      changedelete = { text = '' },
     },
+    signs_staged = {
+      add = { text = '' },
+      change = { text = '' },
+      delete = { text = '' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '' },
+    },
+    signs_staged_enable = true,
+    auto_attach = true,
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'eol',
+      delay = 500,
+      ignore_whitespace = false,
+      use_focus = true,
+    },
+    current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
   },
 }
